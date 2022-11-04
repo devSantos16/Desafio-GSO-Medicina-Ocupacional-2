9dd00e6b08f4fa1f717b01e571ccfba53507d17b
devsantos16 <devsantos1999@gmail.com>
2022-11-04 13:24:51 -0300

    Adicionando popup, footer e comentarios

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,9 +9,28 @@ buttonMaior.addEventListener("click", () => {
 
 buttonMenor.addEventListener("click", () => {
     let input = parseInt(document.getElementById("input").value) - 1;
+    if (input < 0) {
+        mostrarPopup();
+        return;
+    }
     document.getElementById("input").value = input;
 })
 
 resetar.addEventListener("click", () => {
     document.getElementById("input").value = 0;
 })
+
+async function mostrarPopup() {
+    var popup = document.getElementById("popup");
+
+    if (document.getElementsByClassName("hidden")) {
+        await popup.classList.remove("hidden");
+    }
+    
+    popup.classList.toggle("show");
+    await new Promise(r => setTimeout(r, 2000));
+    await popup.classList.toggle("hidden");
+
+    popup.classList.remove("show");
+
+}
